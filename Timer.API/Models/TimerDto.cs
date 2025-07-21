4f19ea897cbdf15a58ea02c9759cf8570d89625f
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-07-21 23:55:47 +0300

    Merge feature/setup-ui into main

---
+++ Timer.API/Models/TimerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Timer.API.Models
+{
+    public class TimerDto
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int RemainingSeconds { get; set; }
+        public int DurationInSeconds { get; set; }
+        public bool IsPaused { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
